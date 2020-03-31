55ccbe1e052c86f8fa18393465c5c8373d02eb1d
Edwin U Kannanaikkal <edwinforedwin@gmail.com>
2020-03-31 17:07:35 +0530

    Create Trend and Seasonality.py

---
+++ Understanding Time Series Features/Various Models/Trend and Seasonality.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from pandas.plotting import autocorrelation_plot
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error 
+from math import sqrt
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt 
+import statsmodels.api as sm 
+from statsmodels.tsa.stattools import adfuller 
+from matplotlib.pylab import rcParams 
+from statsmodels.tsa.seasonal import seasonal_decompose 
+from statsmodels.tsa.stattools import acf, pacf
+from statsmodels.tsa.arima_model import ARIMA
+
+test = pd.read_excel(r'C:\Users\Edwin U Kannanaikkal\Documents\MISHMASH Challenge\Booked Attibute.xlsx')
+train = pd.read_excel(r'C:\Users\Edwin U Kannanaikkal\Documents\MISHMASH Challenge\Training-Data-Sets.xlsx')
+
+df_test = pd.DataFrame(test)
+df_train = pd.DataFrame(train)
+
+Train = df_train[0:11000]
+Valid = df_train[11000:12001]
+
+plt.figure(figsize=(14,7))
+sm.tsa.seasonal_decompose(Train.EQ.values,period=28).plot() 
+result = sm.tsa.stattools.adfuller(train.EQ) 
+plt.show()
