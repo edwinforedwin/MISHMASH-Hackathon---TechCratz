0ae84f391346a94a5f0308045ba13db7cf61fb6f
Edwin U Kannanaikkal <edwinforedwin@gmail.com>
2020-03-31 17:05:51 +0530

    Create moving average.py

---
+++ Understanding Time Series Features/Various Models/moving average.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from pandas.plotting import autocorrelation_plot
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error 
+from math import sqrt
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt 
+import statsmodels.api as sm 
+from statsmodels.tsa.stattools import adfuller 
+from matplotlib.pylab import rcParams 
+from statsmodels.tsa.seasonal import seasonal_decompose 
+from statsmodels.tsa.stattools import acf, pacf
+from statsmodels.tsa.arima_model import ARIMA
+
+test = pd.read_excel(r'C:\Users\Edwin U Kannanaikkal\Documents\MISHMASH Challenge\Booked Attibute.xlsx')
+train = pd.read_excel(r'C:\Users\Edwin U Kannanaikkal\Documents\MISHMASH Challenge\Training-Data-Sets.xlsx')
+
+df_test = pd.DataFrame(test)
+df_train = pd.DataFrame(train)
+
+Train = df_train[0:11000]
+Valid = df_train[11000:12001]
+
+#Moving Average method
+
+y_hat_avg = Valid.copy() 
+y_hat_avg['moving_avg_forecast'] = Train['EQ'].rolling(50).mean().iloc[-1] 
+# average of last 10 observations. 
+plt.figure(figsize=(15,5)) 
+plt.plot(Train['EQ'], label='Train') 
+plt.plot(Valid['EQ'], label='Valid') 
+plt.plot(y_hat_avg['moving_avg_forecast'], label='Moving Average Forecast using 50 observations') 
+plt.legend(loc='best') 
+plt.show() 
+rms = sqrt(mean_squared_error(Valid.EQ, y_hat_avg.moving_avg_forecast)) 
+print(rms)
